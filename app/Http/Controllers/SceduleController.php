afb415ee63a358e3bf15bbc3d82da7d123cf5b36
efriza <Efrizals@it.student.pens.ac.id>
2022-06-09 22:13:47 +0700

    jadi

--- app/Http/Controllers/SceduleController.php
+++ app/Http/Controllers/SceduleController.php
@@ -10,7 +10,6 @@ class SceduleController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * 
      * @return \Illuminate\Http\Response
      */
     public function index()
